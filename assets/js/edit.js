3e689e69d0419be25c2ac0e46e4e6a7596b53c8c
Yumi Kim <yumikimsk@gmail.com>
2016-04-04 13:41:26 -0400

    add some javascript for animation

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -1,8 +1,54 @@
+var menuShowing = true;
+var visWidth = 2048;
+var visHeight = 1536;
+
 $(document).ready(function() {
 	$('#hide').click(toggleMenu);
 	$('#show').click(toggleMenu);
+
+	toggleVisualizationSize();
 });
 
 function toggleMenu() {
+	if (menuShowing == true) {
+		menuShowing = false;
+	} else {
+		menuShowing = true;
+	}
+
 	$("#menu").toggle("slide");
+	toggleVisualizationSize();
+}
+
+function toggleVisualizationSize() {
+	var bigWidth = $(this).width();
+	var smallWidth = bigWidth - 460;
+	var windowHeight = $(this).height();
+	var ratio = visWidth/visHeight;
+
+	if (menuShowing) {
+		// make smaller
+		var ratio = smallWidth/visWidth;
+		var smallHeight = visHeight * ratio;
+		if (smallHeight > windowHeight) {
+			console.log("bigger");
+			ratio = windowHeight/visHeight;
+			var smallWidth = visWidth * ratio;
+			var smallHeight = windowHeight;
+		}
+		$("#visualization").animate({width:smallWidth}, { duration: 300, queue: false });
+		$("#visualization").animate({height:smallHeight}, { duration: 300, queue: false });
+		$("#visualization").animate({height:smallHeight}, { duration: 300, queue: false });
+		$("#visualization").animate({height:smallHeight}, { duration: 300, queue: false });
+	} else {
+		var bigHeight = visHeight * ratio;
+		var ratio = bigWidth/bigHeight;
+		if (bigHeight > windowHeight) {
+			ratio = windowHeight/visHeight;
+			var bigWidth = visWidth * ratio;
+			var bigHeight = windowHeight;
+		}
+		$("#visualization").animate({width:bigWidth}, { duration: 300, queue: false });
+		$("#visualization").animate({height:bigHeight}, { duration: 300, queue: false });
+	}
 }
