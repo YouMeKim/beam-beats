1a0ef00e5f4e2bbadac6a7760d6cf9edf877a4b9
Yumi Kim <yumikimsk@gmail.com>
2016-04-12 18:31:30 -0400

    add instructions page layout list page

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+//var currentStep = 1;
+var currentStep = 3;
+
+$(document).ready(function() {
+    $('#welcome-content').click(nextStep);
+    $('#instructions-button').click(nextStep);
+
+    //moveTo("welcome");
+    moveTo("list");
+});
+
+function nextStep() {
+    currentStep++;
+    switch(currentStep) {
+        case 1:
+            moveTo("welcome");
+            break;
+        case 2:
+            moveTo("instructions");
+            break;
+        case 3:
+            moveTo("list");
+            break;
+        default:
+            moveTo("welcome");
+            currentStep = 1;
+            break;
+    }
+}
+
+function openWelcomePage() {
+    console.log("welcome page");
+}
+
+function openInstructionPage() {
+    console.log("instruction page");
+}
+
+function moveTo(sectionName) {
+    $('#' + sectionName).fadeIn(300).siblings().hide();
+}
