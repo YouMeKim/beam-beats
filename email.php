3d08107e7dd70a9f749bb4efe8d6e472901aaa09
Yumi Kim <yumikimsk@gmail.com>
2016-05-02 15:20:41 -0400

    add styles to email

--- email.php
+++ email.php
@@ -7,7 +7,7 @@ echo "Sending email to " . $email . " with visualization id: " . $id;
 
 $to = $email;
 $subject = "Beam Beats - Your Visualization";
-$message = "<html><body><h1>Your Beam Beats Visualization (" . $id . ")</h1><img style='width: 500px; height: 500px; margin: 0px auto;' alt='visualization' src='http://beambeats.cias.rit.edu/visualization/assets/vis/" . $id . ".png'></body></html>";
+$message = "<html><body><h1>Your Beam Beats Visualization (" . $id . ")</h1><img style='width: 500px; height: 500px; margin: 0px auto; background-color: black;' alt='visualization' src='http://beambeats.cias.rit.edu/visualization/assets/vis/" . $id . ".png'></body></html>";
 $headers = "From: " . strip_tags($sender) . "\n";
 $headers .= "Reply-To: ". strip_tags($sender) . "\n";
 $headers .= "MIME-Version: 1.0\n";
